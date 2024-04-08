0d3af7aace998aea2bdf4a9979e337cae561291e
Telvin <mark.telvin@student.moringaschool.com>
2024-04-08 12:45:58 +0300

    jy

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+    // Change the text content of the page
+    document.getElementById("text").textContent = "This is really cool!";
+});
